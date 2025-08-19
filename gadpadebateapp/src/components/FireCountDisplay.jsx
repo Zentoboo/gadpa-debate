0ba3ecbd7ac916d3e64c1a9682a2c53dfae93389
Christopher Bertrand <c.bertrandtjo@gmail.com>
2025-08-19 21:59:07 +0800

    FEAT: changed refresh to working svg (current issue button background of refresh is red on hover which is bad)

--- gadpadebateapp/src/components/FireCountDisplay.jsx
+++ gadpadebateapp/src/components/FireCountDisplay.jsx
@@ -16,33 +16,34 @@ export default function FireCountDisplay({ token }) {
 
     const refreshFireCount = () => {
         authFetch("http://localhost:5076/debate-manager/live/status")
-            .then(res => {
-                if (!res.ok) throw new Error("Failed to fetch live status");
-                return res.json();
-            })
+            .then(res => res.json())
             .then(data => {
-                if (data.isLive) {
-                    setTotalFires(data.totalFires);
-                }
+                if (data.isLive) setTotalFires(data.totalFires);
             })
-            .catch(err => console.error(err));
+            .catch(console.error);
     };
 
     useEffect(() => {
         refreshFireCount();
-        const interval = setInterval(refreshFireCount, 5000); // Poll every 5 seconds
+        const interval = setInterval(refreshFireCount, 5000);
         return () => clearInterval(interval);
     }, [token]);
 
     return (
         <div className="live-debate-fires">
             🔥 {totalFires}
-            <button
-                onClick={refreshFireCount}
-                className="refresh-button"
-                title="Refresh fires"
-            >
-                🔄
+            <button className="refresh-button">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 30 30"
+                    width="24"
+                    height="24"
+                    className="refresh-icon"
+                >
+                    <path d="M 15 3 C 12.031398 3 9.3028202 4.0834384 7.2070312 5.875 A 1.0001 1.0001 0 1 0 8.5058594 7.3945312 C 10.25407 5.9000929 12.516602 5 15 5 C 20.19656 5 24.450989 8.9379267 24.951172 14 L 22 14 L 26 20 L 30 14 L 26.949219 14 C 26.437925 7.8516588 21.277839 3 15 3 z M 4 10 L 0 16 L 3.0507812 16 C 3.562075 22.148341 8.7221607 27 15 27 C 17.968602 27 20.69718 25.916562 22.792969 24.125 A 1.0001 1.0001 0 1 0 21.494141 22.605469 C 19.74593 24.099907 17.483398 25 15 25 C 9.80344 25 5.5490109 21.062074 5.0488281 16 L 8 16 L 4 10 z"
+                        fill="currentColor" />
+                </svg>
+
             </button>
         </div>
     );

f3559b0bdf093a6267950b55a80cc5d53190b21a
Christopher Bertrand <c.bertrandtjo@gmail.com>
2025-10-02 05:56:05 +0800

    FIX (ui): fixed ui issues on about page ensuring responsiveness and maintained width size

--- gadpadebateapp/src/pages/About.jsx
+++ gadpadebateapp/src/pages/About.jsx
@@ -94,7 +94,7 @@ export default function About() {
                 </div>
 
                 <div class="video-embed">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/4BDlXXR1vnQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen ></iframe>
+                    <iframe src="https://www.youtube.com/embed/4BDlXXR1vnQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen ></iframe>
                 </div>
             </FadeInSection>
             <FadeInSection className="content-section">
